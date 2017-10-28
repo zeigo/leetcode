b1f1ed9a9b87b3d8d540d71decb7f8ed92cc87eb
zeigo <zjj1149@gmail.com>
2017-10-28 11:10:16 +0800

    309. Best Time to Buy and Sell Stock with Cooldown

--- Algorithms/309. Best Time to Buy and Sell Stock with Cooldown .js
+++ Algorithms/309. Best Time to Buy and Sell Stock with Cooldown .js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+  let len = prices.length,
+      has1Sell = 0,
+      has1None = -prices[0],
+      has0Buy = -prices[0],
+      has0None = 0;
+  for(let i = 1; i < len; i++){
+    let p = prices[i];
+    has1None = Math.max(has1None, has0Buy);
+    has0Buy = has0None - p;
+    has0None = Math.max(has1Sell, has0None);
+    has1Sell = has1None + p;
+  }
+  return Math.max(has1Sell, has0None);
 };
