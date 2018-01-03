6cf789ad4a16f8b1c171cbb8dd827b42d21db994
zeigo <zjj1149@gmail.com>
2018-01-03 16:16:40 +0800

    fix quicksort, dont need swap

--- Basic/QuickSort.js
+++ Basic/QuickSort.js
@@ -1,9 +1,4 @@
 var quickSort = function(list){
-  function swap(a, b){ // 交换a,b索引处值
-    let tmp = list[a];
-    list[a] = list[b];
-    list[b] = tmp;
-  }
   function helper(left, right){ // 以list[left]值为基准，分割小大两部分
     if(left >= right) return ;
     let val = list[left],
@@ -11,10 +6,10 @@ var quickSort = function(list){
         j = right;
     while(i < j){
       // 保持list[i] <= val <= list[j]
-      while(i < j && val < list[j]) j--;
-      swap(i, j);
-      while(i < j && val > list[i]) i++;
-      swap(i, j);
+      while(i < j && list[j] >= val) j--;
+      list[i] = list[j];
+      while(i < j && list[i] <= val) i++;
+      list[j] = list[i];
     }
     list[i] = val;
     helper(left, i - 1);
