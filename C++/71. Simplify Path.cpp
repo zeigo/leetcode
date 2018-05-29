be46d7f4a5b8c54d278e11fe3b56e6b4bf02c93d
zeigo <zjj1149@gmail.com>
2018-05-29 11:18:22 +0800

    71. Simplify Path.cpp

---
+++ C++/71. Simplify Path.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ans, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss, tmp, '/')) {
+            if(tmp == "." || tmp == "") continue;
+            if(tmp != "..")
+                stk.push_back(tmp);
+            else if(!stk.empty())
+                stk.pop_back();
+       }
+       for(auto str : stk) {
+           ans += "/" + str;
+       }
+       return ans;
+    }
+};
